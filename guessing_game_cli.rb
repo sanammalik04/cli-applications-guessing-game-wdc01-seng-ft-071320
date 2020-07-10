4394c4eea9d743d252732af8d03b8bff5dd623ee
Sanam Malik <sanam.malik04@gmail.com>
2020-07-10 01:23:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,13 @@
 # Code your solution here!
 def run_guessing_game
-  rand(6) + 1
+  rightAnswer = rand(6) + 1
+  puts "Type your guess! "
+  userGuess = gets.chomp
+  if userGuess == rightAnswer then
+    puts "You guessed the correct number!"
+  elsif userGuess == "exit" then
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{rightAnswer}."
+  end
+end
